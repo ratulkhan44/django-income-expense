285e38091130f92419d2ea5c76ecd79c6039d9fa
ratulkhan44 <ratulcse.khan@gmail.com>
2020-10-01 00:22:51 +0600

    Working authentication

---
+++ authentication/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from authentication.views import RegistrationView
+
+
+app_name= 'authentication'
+
+urlpatterns = [
+    path('register/', RegistrationView.as_view(), name='register'),
+]
